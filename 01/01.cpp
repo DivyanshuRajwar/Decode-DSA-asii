8472561a95084736d250129242bfacf7803dc27a
DivyanshuRajwar <divyanshurajwar@gmail.com>
2024-04-02 12:23:30 +0530

    Assignment-01

---
+++ 01/01.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+// question 1
+    cout<<"Question1"<<endl<<"physics "<<endl<<"Wallah"<<endl;
+// QUESTION 2
+    cout<<"Question2"<<endl<<endl<<5+5<<endl;
+// question 3
+    cout<<"Question3"<<endl<<"the size of int is :"<<sizeof(int);
+    cout<<"the size of bool is :"<<sizeof(bool);
+    cout<<"the size of Float is :"<<sizeof(float);
+    cout<<"the size of double is :"<<sizeof(double)<<endl;
+// question 4
+    cout<<"Question4"<<endl;
+    int a = 4;
+    int b = 5;
+    a++, b--;
+    cout <<endl<< ++a << " " << b--<<endl;
+//question 5
+    cout<<"Question5"<<endl;
+    float pi=3.1415,radious=10;
+    cout<<endl<<2*pi*radious<<endl;
+    
+return 0;
+}
---
+++ 01/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+cout<<"Question4"<<endl;
+    int a = 4;
+    int b = 5;
+    a++, b--;
+    cout <<endl<< ++a << " " << b--<<endl;
